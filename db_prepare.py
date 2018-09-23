7f51f7997fe87c4c9a11f79b9b9cb64e95099128
Tom <tom.hirshberg@gmail.com>
2018-09-23 15:34:36 +0300

    updated db_prepare with total wins and podiums till current race

--- db_prepare.py
+++ db_prepare.py
@@ -18,30 +18,76 @@ for d in dict:
 print(len(dict))
 """
 
-# in results, instead of statusId, put the corresponding statusBinary
 results = []
-count = 0
+# count = 0 # counter for debugging
 with open('formula_DB/results.csv','r') as f:
     reader = csv.reader(f)
     for row in reader:
+        # in results, instead of statusId, put the corresponding statusBinary
         if row[0] != 'resultId':
             row[17] = dict_status[row[17]]  # row[17] contains the statusId. convert to statusBinary
         results.append(row)  # after changes, including the header
-        count+=1
-        if count>10:
-           break
+        # count+=1
+        # if count>50:
+        #    break
 
+"""
+compute the number of driver-involved-status until the current race
+append that number to the end of each row in results (new column "counterInvolvedProblems")
+compute the number of wins and podiums(positions 1-3) until the current race
+append that numbers to the end of each row in results (new columns "wins","podiums")
+"""
 prob = {}
-for i in range(len(results)-1, 1, -1):
+wins = {}
+podiums = {}
+for i in range(len(results)-1, 0, -1):
     row = results[i]
-    if row[17]==1: # driver involved (status)
-        driver = row[2] #row[2] is driverId
+    driver = row[2]
+    position = row[6]
+    statusId = row[17]
+
+    # implement: driver involved problem counter
+    if statusId == 1:  # driver involved (status)
         if driver in prob:
             prob[driver] += 1
         else:
             prob[driver] = 1
+    if driver in prob:
         row.append(prob[driver])
     else:
         row.append(0)
-(results[0]).append('counterInvolved')
-# print(results)
+
+    # implement: podiums and wins counters
+    if position == '1':  # win
+        if driver in wins:
+            wins[driver] += 1
+            podiums[driver] += 1  # sure in podiums if won before
+        else:
+            wins[driver] = 1
+            if driver in podiums:
+                podiums[driver] += 1
+            else:
+                podiums[driver] = 1
+    if position in ['2','3']:  # podiums
+        if driver in podiums:
+            podiums[driver] += 1
+        else:
+            podiums[driver] = 1
+    if driver in wins:
+        row.append(wins[driver])
+    else:
+        row.append(0)
+    if driver in podiums:
+        row.append(podiums[driver])
+    else:
+        row.append(0)
+
+(results[0]).append('counterInvolvedProblems')
+(results[0]).append('wins')
+(results[0]).append('podiums')
+
+"""
+# self-check
+for i in range(len(results)):
+    print(results[i])
+"""
