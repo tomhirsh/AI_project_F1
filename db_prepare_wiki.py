a62e1c380a05f72c36d957206ebe5aa4578feb31
Tom <tom.hirshberg@gmail.com>
2018-09-23 18:50:28 +0300

    added number of fastest laps and start to the drivers, for the wiki DB

--- db_prepare_wiki.py
+++ db_prepare_wiki.py
@@ -20,17 +20,22 @@ with open('formula_DB/drivers_edited.csv', 'r', encoding="utf8") as f:
         #count+=1
         #if count>10:
         #    break
-#
 
+wiki_starts = {}
+wiki_fastest_laps={}
 with open('formula_DB/wiki_drivers_edited.csv','r', encoding="utf8") as f:
     reader = csv.reader(f)
     for row in reader:
         driver_name = row[0]
         starts = row[5]
-        if driver_name in full_names:
-            driver_row = int(full_names[driver_name])
-            drivers[driver_row].append(starts)
-            #print(driver_name)
-        else:
-            print(driver_name)
+        fastest_laps = row[9]
+        wiki_starts[driver_name] = starts
+        wiki_fastest_laps[driver_name] = fastest_laps
 
+for row in drivers:
+    driver_name = row[9]
+    if driver_name in wiki_starts:
+        row.append(int(wiki_starts[driver_name]))
+        row.append(int(wiki_fastest_laps[driver_name]))
+    else:
+        row.append(-1)
