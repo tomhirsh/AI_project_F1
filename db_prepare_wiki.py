48aa9de65de9ba8b70616aeca8553f17a6a0765b
Tom <tom.hirshberg@gmail.com>
2018-10-20 09:04:22 +0300

    made db_prepare_wiki by modules - we can use public funcs to get information for each driver (by its id)

--- db_prepare_wiki.py
+++ db_prepare_wiki.py
@@ -1,76 +1,116 @@
 import csv
 import db_prepare_countries
 
-full_names = {}
-drivers = []
-#count = 0
-with open('formula_DB/drivers_edited.csv', 'r', encoding="utf8") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        driverId = row[0]
-        first_name = row[4]
-        last_name = row[5]
-        if driverId != 'driverId':
-            full_name = first_name+" "+last_name
-            full_names[full_name] = driverId
-            row.append(full_name)
+def prepare_drivers():
+    full_names = {}
+    drivers = []
+    #count = 0
+    with open('formula_DB/drivers_edited.csv', 'r', encoding="utf8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            driverId = row[0]
+            first_name = row[4]
+            last_name = row[5]
+            if driverId != 'driverId':
+                full_name = first_name+" "+last_name
+                full_names[full_name] = driverId
+                row.append(full_name)
+            else:
+                row.append('fullName') # header
+            drivers.append(row)
+            #count+=1
+            #if count>10:
+            #    break
+
+    wiki_starts = {}
+    wiki_fastest_laps = {}
+    wiki_countries = {}
+    with open('formula_DB/wiki_drivers_edited.csv','r', encoding="utf8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            driver_name = row[0]
+            starts = row[5]
+            fastest_laps = row[9]
+            country = row[1]
+            wiki_starts[driver_name] = starts
+            wiki_fastest_laps[driver_name] = fastest_laps
+            wiki_countries[driver_name] = country
+
+    counter = 0
+    for row in drivers:
+        if counter == 0:
+            row.append("starts_num")
+            row.append("fastest_laps_num")
+            row.append("country")
+            counter += 1
+            continue
+        driver_name = row[9]
+        if driver_name in wiki_starts:
+            row.append(int(wiki_starts[driver_name]))
+            row.append(int(wiki_fastest_laps[driver_name]))
+            row.append(wiki_countries[driver_name])
         else:
-            row.append('fullName') # header
-        drivers.append(row)
-        #count+=1
-        #if count>10:
-        #    break
+            row.append(-1)
+            row.append(-1)
+            row.append(-1)
 
-wiki_starts = {}
-wiki_fastest_laps = {}
-wiki_countries = {}
-with open('formula_DB/wiki_drivers_edited.csv','r', encoding="utf8") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        driver_name = row[0]
-        starts = row[5]
-        fastest_laps = row[9]
-        country = row[1]
-        wiki_starts[driver_name] = starts
-        wiki_fastest_laps[driver_name] = fastest_laps
-        wiki_countries[driver_name] = country
+    """
+    use db_prepare_countries to get the rank of a driver's country and append it to the driver
+    """
+    counter = 0
+    for row in drivers:
+        if counter == 0:
+            row.append("country_rank_by_championships")
+            row.append("country_rank_by_proportion_drivers")
+            counter += 1
+            continue
+        country_name = row[12]
+        if country_name != -1:
+            row.append(db_prepare_countries.get_rank_by_championships(country_name))
+            row.append(db_prepare_countries.get_rank_by_proportion_champs_drivers(country_name))
+        else:
+            row.append(-1)
+            row.append(-1)
 
-counter = 0
-for row in drivers:
-    if counter == 0:
-        row.append("starts_num")
-        row.append("fastest_laps_num")
-        row.append("country")
-        counter += 1
-        continue
-    driver_name = row[9]
-    if driver_name in wiki_starts:
-        row.append(int(wiki_starts[driver_name]))
-        row.append(int(wiki_fastest_laps[driver_name]))
-        row.append(wiki_countries[driver_name])
-    else:
-        row.append(-1)
-        row.append(-1)
-        row.append(-1)
-
-"""
-use db_prepare_countries to get the rank of a driver's country and append it to the driver
-"""
-counter = 0
-for row in drivers:
-    if counter == 0:
-        row.append("country_rank_by_championships")
-        row.append("country_rank_by_proportion_drivers")
-        counter += 1
-        continue
-    country_name = row[12]
-    if country_name != -1:
-        row.append(db_prepare_countries.get_rank_by_championships(country_name))
-        row.append(db_prepare_countries.get_rank_by_proportion_champs_drivers(country_name))
-    else:
-        row.append(-1)
-        row.append(-1)
+    """
+    handling the age of a driver - in order to add it to the race's db (age in a race)
+    """
+    counter = 0
+    for row in drivers:
+        if counter == 0:
+            row.append("year_of_birth")
+            counter += 1
+            continue
+        dob = row[6]
+        _,_,yob = dob.split("/")
+        row.append(yob)
+    print(drivers[0])
+    print(drivers[1])
+    return drivers
 
 
-print(drivers[0])
-print(drivers[1])
+drivers = prepare_drivers()
+
+
+def get_starts_num_by_driver(driver_id):
+    return drivers[driver_id][10]
+
+
+def get_fastest_laps_num_by_driver(driver_id):
+    return drivers[driver_id][11]
+
+
+def get_country_rank_championships_by_driver(driver_id):
+    return drivers[driver_id][13]
+
+
+def get_country_rank_proportion_by_driver(driver_id):
+    return drivers[driver_id][14]
+
+
+def get_year_of_birth_of_driver(driver_id):
+    return drivers[driver_id][15]
+
+
+#print(get_country_rank_championships_by_driver(1))
+#print(get_country_rank_proportion_by_driver(1))
